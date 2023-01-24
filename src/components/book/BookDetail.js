53d5119291c2464fc4c5b5770eb141a78c5c4add
Grey Altr <grey.sifr@gmail.com>
2023-01-23 17:34:14 -0800

    Link in BookDetail.js returns to catalog

--- src/components/book/BookDetail.js
+++ src/components/book/BookDetail.js
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import Book from './Book';
 import { useBookDetail } from '../../hooks/useBookDetail';
 
@@ -15,7 +15,12 @@ function BookDetail() {
 
   if (loading) return <h3>Loading book...</h3>;
 
-  return <Book book={book} showDetail />;
+  return (
+    <>
+      <Book book={book} showDetail />
+      <Link to="/">Back to life... Back to reality...</Link>
+    </>
+  );
 }
 
 export default BookDetail;
